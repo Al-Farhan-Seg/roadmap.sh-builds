48137aaeec45c66dffe57742487324a2e9508099
Al-Farhan-Seg <farhan.segujja@gmail.com>
2026-07-14 23:27:37 +0300

    TABS: added content, responsive styles and first JS workings

--- 10-tabs/script.js
+++ 10-tabs/script.js
@@ -1,4 +1,32 @@
-'use strict';
+const tabButtons = document.querySelectorAll(".tabs")
+const tabContentPairs = {
+    "bio-tab" : "bio-content",
+    "chem-tab" : "chem-content",
+    "phy-tab" : "phy-content",
+    "math-tab" : "math-content"
+}
 
-// Tabs
-// Add project behaviour below.
+tabButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const clickedTabId = event.target.id
+        const relativeContentClass = tabContentPairs[clickedTabId]
+        const contentToDeactivate = []
+
+        Object.values(tabContentPairs).forEach((value) => {
+            if(value !== relativeContentClass){
+                contentToDeactivate.push(value)
+            }
+        })
+        /*for(let i = 0; i < contentToDeactivate.length; i++){
+
+        }*/
+        contentToDeactivate.forEach((value) => {
+            const testElement = document.getElementsByClassName(value)
+            console.log(testElement.classList.value)
+        })
+
+        console.log(`The content of ${clickedTabId} is ${relativeContentClass}`)
+        console.log(`We are going to deactivate ${contentToDeactivate}`);
+        
+    })
+})
