44e112b5063355af835235a2b141b80712ed3761
Al-Farhan-Seg <farhan.segujja@gmail.com>
2026-07-24 22:13:38 +0300

    25-task-tracker: Continued project development
    * Added more CSS styles to make project look better
    * Added the first batch of JavaScript including validation and an Event Listener for the submit button
    
    *** Way forward: complete JS functionality as planned and also refine CSS styles for devices bigger than PHONES

--- 25-task-tracker/script.js
+++ 25-task-tracker/script.js
@@ -1,4 +1,37 @@
-'use strict';
+const titleField = document.getElementById("title-field")
+const descriptionField = document.getElementById("description-field")
+const submitBtn = document.getElementById("submit-btn")
+const pendingTasks = []
+const completedTasks = []
 
-// Task Tracker
-// Add project behaviour below.
+/*
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+}) */
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    if(areBothFieldsFilled()) {
+        
+        // clearInputFields()
+    } else {
+        alertForEmptyFields()
+    }
+})
+
+function areBothFieldsFilled() {
+    const titleValue = titleField.value.trim()
+                                       .split(' ')
+                                       .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                                       .join(' ');
+    const descriptionValue = descriptionField.value.trim()                                 
+    const descriptionValueUpper = descriptionValue.charAt(0).toUpperCase() + descriptionValue.slice(1);                                        
+    console.log(`Title: ${titleValue}, Description: ${descriptionValueUpper}`)
+    return titleValue !== "" && descriptionValue !== "" ? true : false
+}
+function clearInputFields() {
+    titleField.value = ""
+    descriptionField.value = ""
+}
+function alertForEmptyFields() {
+    alert("All fields must be flled to add a new task")
+}
